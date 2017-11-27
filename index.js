41f2d657457291c8362b1442fd124b6353805868
Edie: ) <edienodie@gmail.com>
2017-11-27 23:41:06 +1000

    change greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "mate" });
 });
 
 const PORT = process.env.PORT || 5000;
