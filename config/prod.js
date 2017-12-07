aea7aea7069ff3e79e371a8ccaed49707fa863fe
Edie: ) <edienodie@gmail.com>
2017-12-07 23:45:41 +1000

    Adding billing and client side app

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURL: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  googleRedirectURI: 'http://mighty-coast-33076.herokuapp.com/auth/google',
+
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -9,6 +10,8 @@ require("./services/passport");
 mongoose.connect(keys.mongoURL);
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,6 +22,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
+if(process.env.NODE_ENV ==="production"){
+  //Express will serve up prod assets
+  //like main.js file, or main.css file
+  app.use(Express.static("client/build"));
+
+  //Express will serve up the index.html filw
+  //if it does not recognize the route
+  const path = require('path');
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "click", "build","index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
